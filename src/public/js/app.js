b0d488fba895e2d2af9029ce16c59a81430a67ff
joshyeom <jhyeom95@daum.net>
2024-12-31 22:56:04 +0900

    1.4

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,23 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// socket connected
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+  socket.send("Hello");
+});
+
+// socket disconnected
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+// socket error
+socket.addEventListener("error", (error) => {
+  console.log("Error ❌", error);
+});
+
+// socket.send()
+socket.addEventListener("message", (message) => {
+  console.log(message);
+  console.log(message.data);
+});
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const app = express();
 
@@ -15,4 +18,16 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+function handleConnection(socket) {
+  socket.send("Hello");
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  socket.on("message", (message) => console.log(message));
+}
+
+wss.on("connection", handleConnection);
+
 server.listen(3000, handleListen);
+
+// 클라이언트, 서버 둘 중 하나라도 연결이 끊어지면 다른 하나도 연결이 끊어진다.
+// socket.on() 메서드는 이벤트 리스너를 추가하는 메서드
+// socket.send() 메서드는 메시지를 보내는 메서드
