69bb79d39a5ed2455dac7a2fd8fde6368d642b83
joshyeom <jhyeom95@daum.net>
2025-01-01 00:43:31 +0900

    1.8

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,9 +1,22 @@
+const $messageForm = document.querySelector("#messageForm");
+const $nicknameForm = document.querySelector("#nicknameForm");
+const $message = $messageForm.querySelector("#message");
+const $nickname = $nicknameForm.querySelector("#nickname");
+const $ul = document.querySelector("ul");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+// 메시지 분기처리
+const stringifyMessage = (type, value) => {
+  const msg = {
+    type: type,
+    value: value,
+  };
+  return JSON.stringify(msg);
+};
 // socket connected
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
-  socket.send("Hello");
 });
 
 // socket disconnected
@@ -18,6 +31,22 @@ socket.addEventListener("error", (error) => {
 
 // socket.send()
 socket.addEventListener("message", (message) => {
-  console.log(message);
-  console.log(message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  $ul.appendChild(li);
 });
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  socket.send(stringifyMessage("message", $message.value));
+  $message.value = "";
+};
+
+const handleNicknameSubmit = (event) => {
+  event.preventDefault();
+  socket.send(stringifyMessage("nickname", $nickname.value));
+  $nickname.value = "";
+};
+
+$messageForm.addEventListener("submit", handleSubmit);
+$nicknameForm.addEventListener("submit", handleNicknameSubmit);
--- src/server.js
+++ src/server.js
@@ -18,16 +18,28 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 function handleConnection(socket) {
-  socket.send("Hello");
+  sockets.push(socket);
   socket.on("close", () => console.log("Disconnected from Browser ❌"));
-  socket.on("message", (message) => console.log(message));
+  socket.on("message", (message) => {
+    const { type, value } = JSON.parse(message);
+    switch (type) {
+      case "nickname":
+        socket.nickname = value;
+        break;
+      case "message":
+        sockets.forEach((eachSocket) =>
+          eachSocket.send(`${socket.nickname}: ${value}`)
+        );
+        break;
+      default:
+        break;
+    }
+  });
 }
 
 wss.on("connection", handleConnection);
 
 server.listen(3000, handleListen);
-
-// 클라이언트, 서버 둘 중 하나라도 연결이 끊어지면 다른 하나도 연결이 끊어진다.
-// socket.on() 메서드는 이벤트 리스너를 추가하는 메서드
-// socket.send() 메서드는 메시지를 보내는 메서드
